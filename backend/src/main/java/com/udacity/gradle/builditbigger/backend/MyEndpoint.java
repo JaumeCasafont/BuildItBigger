0802dfa717311f548e485e3d3b55b57cd5e15c71
JaumeCasafont <jaucr94@gmail.com>
2018-04-29 02:25:03 +0200

    setup GCE

--- backend/src/main/java/com/udacity/gradle/builditbigger/backend/MyEndpoint.java
+++ backend/src/main/java/com/udacity/gradle/builditbigger/backend/MyEndpoint.java
@@ -3,13 +3,14 @@ package com.udacity.gradle.builditbigger.backend;
 import com.google.api.server.spi.config.Api;
 import com.google.api.server.spi.config.ApiMethod;
 import com.google.api.server.spi.config.ApiNamespace;
+import com.jcr.javajokes.Joker;
 
 import javax.inject.Named;
 
 /** An endpoint class we are exposing */
 @Api(
         name = "myApi",
-        version = "v1",
+        version = "v2",
         namespace = @ApiNamespace(
                 ownerDomain = "backend.builditbigger.gradle.udacity.com",
                 ownerName = "backend.builditbigger.gradle.udacity.com",
@@ -23,6 +24,14 @@ public class MyEndpoint {
     public MyBean sayHi(@Named("name") String name) {
         MyBean response = new MyBean();
         response.setData("Hi, " + name);
+
+        return response;
+    }
+
+    @ApiMethod(name = "tellJoke")
+    public MyBean tellJoke() {
+        MyBean response = new MyBean();
+        response.setData(new Joker().getJoke());
 
         return response;
     }
